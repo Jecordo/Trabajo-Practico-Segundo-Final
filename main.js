78472c4df1388b5b6bbdd33a51ef47e62d289886
Jecordo <jesuscordoba987@gmail.com>
2023-12-03 17:07:05 -0300

    Genrar imagen

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-document.getElementById('generateImage').addEventListener('click', function () {
-    // Lógica para generar la imagen y mostrarla en #generatedImageContainer
-    console.log('Imagen generada');
+
+var imagesArray = ["image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg", "image5.jpg"];
+
+function generateImage() {
+    var randomIndex = Math.floor(Math.random() * imagesArray.length);
+    
+    var randomImage = imagesArray[randomIndex];
+
+    document.getElementById('generatedImage').src = randomImage;
+}
+
+document.getElementById('generateButton').addEventListener('click', function () {
+    generateImage();
+    console.log('Botón de generar imagen clickeado');
 });
+
+var songContainers = document.getElementsByClassName('song_container');
+for (var i = 0; i < songContainers.length; i++) {
+    songContainers[i].addEventListener('click', function () {
+        console.log('Canción clickeada');
+    });
+}
