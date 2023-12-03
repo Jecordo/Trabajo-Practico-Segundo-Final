8fe5345eae1e771cb25205bacd35c6a4c7465b1d
Jecordo <jesuscordoba987@gmail.com>
2023-12-03 16:42:00 -0300

    Commit de inicio

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('generateImage').addEventListener('click', function () {
+    // Lógica para generar la imagen y mostrarla en #generatedImageContainer
+    console.log('Imagen generada');
+});
---
+++ songs.js
@@ -0,0 +1,12 @@
+const songs = [
+    { artist: 'Artist 1', song: 'Canción 1', image: 'image1.jpg' },
+    { artist: 'Artist 2', song: 'Canción 2', image: 'image2.jpg' },
+    { artist: 'Artist 3', song: 'Canción 3', image: 'image3.jpg' },
+    { artist: 'Artist 4', song: 'Canción 4', image: 'image4.jpg' },
+    { artist: 'Artist 5', song: 'Canción 5', image: 'image5.jpg' },
+    { artist: 'Artist 6', song: 'Canción 6', image: 'image6.jpg' },
+    { artist: 'Artist 7', song: 'Canción 7', image: 'image7.jpg' },
+    { artist: 'Artist 8', song: 'Canción 8', image: 'image8.jpg' },
+    { artist: 'Artist 9', song: 'Canción 9', image: 'image9.jpg' },
+    { artist: 'Artist 10', song: 'Canción 10', image: 'image10.jpg' }
+];
