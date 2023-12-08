707bc3ba07ce71f99e772659d9f55a50733191d2
Jecordo <jesuscordoba987@gmail.com>
2023-12-08 13:01:23 -0300

    Generar imagen

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 function generateImage() {
     var captureElement = document.body;
   
+    console.log('Definiendo tamaño de imagen');
+
     var captureOptions = {
       x: captureElement.offsetWidth / 4,  
       y: 10,
       width: 800, 
       height: 590     
+
     };
   
     html2canvas(captureElement, {...captureOptions, useCORS: true }).then(function(canvas) {
       var generatedImage = document.getElementById('generatedImage');
       generatedImage.src = canvas.toDataURL();
+      console.log('Imagen generada');
     });
   }
   
--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('generateImage').addEventListener('click', function () {
-    // Lógica para generar la imagen y mostrarla en #generatedImageContainer
-    console.log('Imagen generada');
-});
