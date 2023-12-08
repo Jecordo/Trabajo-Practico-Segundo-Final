40b9b212080e1421168826681926157e851d2289
Jecordo <jesuscordoba987@gmail.com>
2023-12-08 12:56:13 -0300

    Generar imagen

--- main.js
+++ main.js
@@ -1,22 +1,16 @@
-
-var imagesArray = ["Portadas/image1.jpg", "Portadas/image2.jpg", "Portadas/image3.jpg", "Portadas/image4.jpg", "Portadas/image5.jpg"];
-
 function generateImage() {
-    var randomIndex = Math.floor(Math.random() * imagesArray.length);
-    
-    var randomImage = imagesArray[randomIndex];
-
-    document.getElementById('generatedImage').src = randomImage;
-}
-
-document.getElementById('generateButton').addEventListener('click', function () {
-    generateImage();
-    console.log('Botón de generar imagen clickeado');
-});
-
-var songContainers = document.getElementsByClassName('song_container');
-for (var i = 0; i < songContainers.length; i++) {
-    songContainers[i].addEventListener('click', function () {
-        console.log('Canción clickeada');
+    var captureElement = document.body;
+  
+    var captureOptions = {
+      x: captureElement.offsetWidth / 4,  
+      y: 10,
+      width: 800, 
+      height: 590     
+    };
+  
+    html2canvas(captureElement, {...captureOptions, useCORS: true }).then(function(canvas) {
+      var generatedImage = document.getElementById('generatedImage');
+      generatedImage.src = canvas.toDataURL();
     });
-}
+  }
+  
